966e4015722f6f03cbe1d0b92e0ce468c54feeb1
snqwq <99054951+snqwq@users.noreply.github.com>
2024-01-09 21:49:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const loader = new GLTFLoader();
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
 // load model
