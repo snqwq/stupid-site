1efb469e2c217c47fef6fb270ac3cc0b60e1aaf9
snqwq <99054951+snqwq@users.noreply.github.com>
2024-01-31 09:43:07 -0500

    n

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
 // imports
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // variables
