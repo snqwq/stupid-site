50c76edfd174278855007539ca16965d17ffeb1d
snqwq <99054951+snqwq@users.noreply.github.com>
2024-07-27 17:54:27 -0500

    gays

--- dist/events.js
+++ dist/events.js
@@ -1,4 +1,11 @@
 var date = new Date();
 var currentMonth = date.getMonth() + 1;
 
+pageTitle = document.getElementById("page-title").innerHTML;
+
 console.log(currentMonth);
+console.log(pageTitle);
+
+if (currentMonth === 6) {
+    pageTitle
+}
